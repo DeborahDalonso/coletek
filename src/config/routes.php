1ca0f33176437f385a64795bd2522f5446dc7cbe
Deborah Dalonso <deborahdlns@gmail.com>
2023-11-06 19:25:27 -0300

    arquitetuta

--- src/config/routes.php
+++ src/config/routes.php
@@ -2,17 +2,17 @@
 
 $routes = [];
 
-// Rotas para UserController
+// Rotas User
 $routes['/user'] = ['controller' => 'User', 'action' => 'index'];
 $routes['/user/create'] = ['controller' => 'User', 'action' => 'create'];
 $routes['/user/store'] = ['controller' => 'User', 'action' => 'store'];
 $routes['/user/edit/(\d+)'] = ['controller' => 'User', 'action' => 'edit'];
 $routes['/user/delete/(\d+)'] = ['controller' => 'User', 'action' => 'delete'];
 
-// Rota para a página inicial (home)
+// Rota Main
 $routes['/'] = ['controller' => 'Main', 'action' => 'index'];
 
-// Rota padrão para erro 404
+// Rota Erro HTTP
 $routes['*'] = ['controller' => 'ErrorController', 'action' => 'error404'];
 
 return $routes;
--- src/views/template.php
+++ src/views/template.php
@@ -4,22 +4,22 @@
 <head>
     <title>CRUD de Usuários</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <script src="js/jquery.min.js"></script>
+    <!-- <script src="js/jquery.min.js"></script> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script></head>
 </head>
 
 <body>
     <header>
-        <h2>KALAro</h2>
+        <h2>Bah</h2>
     </header>
 
     <main>
-        <!-- Conteúdo específico da página será inserido aqui -->
+       
     </main>
 
     <footer>
-        <!-- Rodapé com informações gerais aqui -->
+        
     </footer>
 </body>
 
